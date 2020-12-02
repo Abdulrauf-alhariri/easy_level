ab57c5d37f00a89ff2032710eaa23a81c8a11a7b
Abdulrauf-alhariri <74409801+Abdulrauf-alhariri@users.noreply.github.com>
2020-12-02 11:52:59 +0100

    Add files via upload

---
+++ youtube_dw.py
@@ -0,0 +1,53 @@
+from pytube import YouTube
+import urllib.request
+from pathlib import Path
+import os
+
+
+def directory():
+    path = Path(r"C:\Users\abdullrauf.alhariri\Desktop\YoutubeStreams")
+    path.mkdir()
+
+
+def high_quality(yt, path):
+    return yt.streams[0].download(path)
+
+
+def download():
+    path = Path(r"C:\Users\abdullrauf.alhariri\Desktop\YoutubeStreams")
+    try:
+        yt = YouTube(input("Enter the link: "))
+        if True:
+            quality = input(
+                "Do you want to download the highst qualtiy?(yes/ no)").lower()
+            print("")
+
+            if quality == "yes":
+                try:
+                    if True:
+                        high_quality(yt, path)
+                        print("Download is completed")
+
+                except Exception as error:
+                    print(error)
+
+            elif quality == "no":
+                for i in yt.streams.filter(progressive=True):
+                    print(i)
+                try:
+                    stream_itag = int(input("Enter the stream itag: "))
+                    if True:
+                        yt.streams.get_by_itag(stream_itag).download(path)
+                        print("Download is completed")
+
+                except ValueError as v:
+                    print(v)
+
+            else:
+                print("I don't understand, please write correctly")
+
+    except:
+        print("Check if the link is correct!")
+
+
+download()
